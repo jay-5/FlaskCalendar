3c660ff1d18049d0fc99847115bf06f3e529fc92
Jay Jayewardene <raj69@case.edu>
2020-07-25 21:33:57 -0400

    css and html pages

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
  
 app = Flask(__name__)
 
 @app.route("/") # this route is the default one
 def home():
-    return "My name is Jay!"
+    return render_template("home.html")
 
 if  __name__  ==  "__main__":
     app.run(debug=True)
 
+@app.route("/cal")
+def cal():
+    return render_template("cal.html")
