ac7f8f3e8a62e466c7ad426eb7c368d6bf9ab24e
Jay Jayewardene <raj69@case.edu>
2020-07-25 21:19:05 -0400

    commit to 'master'

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ app = Flask(__name__)
 
 @app.route("/") # this route is the default one
 def home():
-    return "My name is Max!"
+    return "My name is Jay!"
 
 if  __name__  ==  "__main__":
     app.run(debug=True)
 
-    
