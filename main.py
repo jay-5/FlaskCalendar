2f7a63ef5a47df635f2f1fe4276fd38a3d1916e1
Jay Jayewardene <raj69@case.edu>
2020-07-27 19:45:26 -0400

    add render_template

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
  
 app = Flask(__name__)
 
-@app.route("/") # this route is the default one
-def home():
-    return render_template("home.html")
-
 if  __name__  ==  "__main__":
     app.run(debug=True)
 
-@app.route("/cal")
+@app.route("/home") # this route goes to the home page
+def home():
+    return render_template("home.html")
+
+@app.route("/cal") # this route goes to the calendar page
 def cal():
     return render_template("cal.html")
